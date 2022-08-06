4b958939ead69142d8ed5e936e18dcda2f8d0c34
bluechen115 <numberdragon01@gmail.com>
2022-08-06 19:30:34 +0900

    Session Option Edited

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 const sessionOption = {
+  resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
   cookie: {
