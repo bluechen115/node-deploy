2062712fee1234c097fb3b6fbebae6b9ee346b7b
bluechen115 <numberdragon01@gmail.com>
2022-08-06 19:31:36 +0900

    Config.js Edited

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "root",
+    username: "moon",
     password: process.env.SEQUELIZE_PASSWORD,
     database: "nodebird",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "root",
+    username: "moon",
     password: process.env.SEQUELIZE_PASSWORD,
     database: "nodebird_test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: "root",
+    username: "moon",
     password: process.env.SEQUELIZE_PASSWORD,
     database: "nodebird",
     host: "127.0.0.1",
